c09d1e680b1b2d33c11899363570be46942055ed
Emanuela Lucaci <emanuela.lucaci26@gmail.com>
2022-11-28 15:17:45 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+            paralelipiped dreptunghic, va afisa volumul lui */
+            Console.WriteLine("Calculam volumul unui paralelipiped dreptunghic.");
+            Console.WriteLine("Introduceti lungime");
+
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latime");
+
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltime");
+
+            int lenght = int.Parse(Console.ReadLine());
+
+            int volume = width * height * lenght;
+            Console.WriteLine("volumul paralelipipedului este: " + volume);
+        }
+    }
+}
